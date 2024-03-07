50da14bd44f2d1879b186b989f1f152694b8578d
Andrew Wang <andrewang2004@gmail.com>
2024-03-06 16:55:10 -0800

    added car model

--- models.js
+++ models.js
@@ -1,6 +1,5 @@
 import {defs, tiny} from './examples/common.js';
 import {Text_Line} from "./examples/text-demo.js";
-
 const {
   Vector, Vector3, vec, vec3, vec4, color, hex_color, Matrix, Mat4, Light, Shape, Material, Scene, Texture,
 } = tiny;
@@ -83,16 +82,90 @@ export function drawChicken(context, program_state, model_transform, dead, wing_
   shapes.cube.draw(context, program_state, model_transform.times(wattle_transform), materials.cube.override({color: vec4(1, 0, 0, 1)}));
 }
 
+export function drawObstacle(context, program_state, model_transform, color) {
+  let height = 0.4; // height of the middle portion
+  let length = 3;
+  let width = 2;
+  let ground_pos = -2 * height;
+
+  const body_transform = Mat4.identity()
+    .times(Mat4.translation(0, 3.5 * height + ground_pos, 0))
+    .times(Mat4.scale(length, height, width));
+
+  const middle_body_transform = body_transform
+    .times(Mat4.scale(1.001, 1.001, 0.4));
+
+  const light_transform = body_transform
+    .times(Mat4.scale(1.002, 0.6, 0.125));
+
+  const light_transform_left = Mat4.translation(0, -0.4 * height, 0.53 * width).times(light_transform);
+  const light_transform_right = Mat4.translation(0, -0.4 * height, -0.53 * width).times(light_transform);
+
+  const lower_body_1 = Mat4.identity()
+    .times(Mat4.translation(0, 2 * height + ground_pos, 0))
+    .times(Mat4.scale(length, 0.5 * height, width));
+
+  const lower_body_2 = Mat4.identity()
+    .times(Mat4.translation(0, height + ground_pos,0))
+    .times(Mat4.scale(length, 0.5 * height, 0.3 * width));
+
+  const lower_body_2_right = Mat4.translation(0, 0, 0.7 * width).times(lower_body_2);
+  const lower_body_2_left = Mat4.translation(0, 0, -0.7 * width).times(lower_body_2);
+
+  const upper_body_transform = Mat4.identity()
+    .times(Mat4.translation(-0.15 * length, 6.25 * height + ground_pos, 0))
+    .times(Mat4.scale(0.5 * length, 1.75 * height, width));
+
+  const side_window_transform = Mat4.translation(0, -0.52 * height, 0)
+    .times(upper_body_transform)
+    .times(Mat4.scale(0.8, 0.7, 1.001));
+
+  const front_back_window_transform = Mat4.translation(0, -0.52 * height, 0)
+    .times(upper_body_transform)
+    .times(Mat4.scale(1.001, 0.7, 0.8));
+
+  const mirror_transform = Mat4.translation(0.03 * length,0.3 * height,0)
+    .times(body_transform)
+    .times(Mat4.scale(0.15, 0.6, 1.2));
+
+  const wheel_transform = body_transform
+    .times(Mat4.scale(0.2, 1.5, 0.301));
+
+  const wheel_front_right_transform = Mat4.translation(0.5 * length, -height * 2.5, 0.7 * width).times(wheel_transform);
+  const wheel_front_right_aux_transform = wheel_front_right_transform.times(Mat4.scale(0.33, 0.33, 1.01));
+  const wheel_front_left_transform = Mat4.translation(0.5 * length, -height * 2.5, -0.7 * width).times(wheel_transform);
+  const wheel_front_left_aux_transform = wheel_front_left_transform.times(Mat4.scale(0.33, 0.33, 1.01));
+  const wheel_back_right_transform = Mat4.translation(-0.5 * length, -height * 2.5, 0.7 * width).times(wheel_transform);
+  const wheel_back_right_aux_transform = wheel_back_right_transform.times(Mat4.scale(0.33, 0.33, 1.01));
+  const wheel_back_left_transform = Mat4.translation(-0.5 * length, -height * 2.5, -0.7 * width).times(wheel_transform);
+  const wheel_back_left_aux_transform = wheel_back_left_transform.times(Mat4.scale(0.33, 0.33, 1.01));
+
+  shapes.cube.draw(context, program_state, model_transform.times(body_transform), materials.plastic.override({color: hex_color("#fd6e30")}));
+  shapes.cube.draw(context, program_state, model_transform.times(middle_body_transform), materials.plastic.override({color: hex_color("#ff4c2b")}));
+  shapes.cube.draw(context, program_state, model_transform.times(light_transform_right), materials.plastic.override({color: hex_color("#ffffff")}));
+  shapes.cube.draw(context, program_state, model_transform.times(light_transform_left), materials.plastic.override({color: hex_color("#ffffff")}));
+  shapes.cube.draw(context, program_state, model_transform.times(lower_body_1), materials.plastic.override({color: hex_color("#ff4c2b")}));
+  shapes.cube.draw(context, program_state, model_transform.times(lower_body_2_left), materials.plastic.override({color: hex_color("#645a8b")}));
+  shapes.cube.draw(context, program_state, model_transform.times(lower_body_2_right), materials.plastic.override({color: hex_color("#645a8b")}));
+  shapes.cube.draw(context, program_state, model_transform.times(upper_body_transform), materials.plastic.override({color: hex_color("#ffffff")}));
+  shapes.cube.draw(context, program_state, model_transform.times(mirror_transform), materials.plastic.override({color: hex_color("#ff4c2b")}));
+  shapes.cube.draw(context, program_state, model_transform.times(side_window_transform), materials.plastic.override({color: hex_color("#000000")}));
+  shapes.cube.draw(context, program_state, model_transform.times(front_back_window_transform), materials.plastic.override({color: hex_color("#000000")}));
+  shapes.cube.draw(context, program_state, model_transform.times(wheel_front_right_transform), materials.plastic.override({color: hex_color("#000000")}));
+  shapes.cube.draw(context, program_state, model_transform.times(wheel_front_left_transform), materials.plastic.override({color: hex_color("#000000")}));
+  shapes.cube.draw(context, program_state, model_transform.times(wheel_back_right_transform), materials.plastic.override({color: hex_color("#000000")}));
+  shapes.cube.draw(context, program_state, model_transform.times(wheel_back_left_transform), materials.plastic.override({color: hex_color("#000000")}));
+  shapes.cube.draw(context, program_state, model_transform.times(wheel_front_right_aux_transform), materials.plastic.override({color: hex_color("#ffffff")}));
+  shapes.cube.draw(context, program_state, model_transform.times(wheel_front_left_aux_transform), materials.plastic.override({color: hex_color("#ffffff")}));
+  shapes.cube.draw(context, program_state, model_transform.times(wheel_back_right_aux_transform), materials.plastic.override({color: hex_color("#ffffff")}));
+  shapes.cube.draw(context, program_state, model_transform.times(wheel_back_left_aux_transform), materials.plastic.override({color: hex_color("#ffffff")}));
+}
+
 export function drawLane(context, program_state, model_transform, color) {
   // shapes.score.set_string("sadaf", context);
   // shapes.score.draw(context, program_state, model_transform, materials.text_image);
   shapes.cube.draw(context, program_state, model_transform, materials.ground.override({color: color}));
 }
-
-export function drawObstacle(context, program_state, model_transform, color) {
-  shapes.cube.draw(context, program_state, model_transform, materials.obstacle.override({color: color}));
-}
-
 export function drawBackground(context, program_state, model_transform) {
   model_transform = Mat4.translation(0, -4, 0)
     .times(model_transform)
